1e06e9af5d6fae87f6a4421fda519cb1337b86c2
Daniel Caires <dan_caires@yahoo.com.br>
2016-11-19 10:17:26 -0200

    Criada matriz de cores e teste de cores

--- DanielCaires5197211.c
+++ DanielCaires5197211.c
@@ -3,7 +3,7 @@
 #include <math.h>
 
 void escreve_pixel(FILE *fp){
-	fprintf(fp,"255 0 0 ");
+	fprintf(fp,"100 0 0 ");
 }
 
 float derivada_numerica(float fsoma, float fdif, float h){
@@ -53,34 +53,33 @@ int main(){
   float a, b, c, d, h, epsilon;
   FILE * arquivo;
   float varteste[2][2];
-  float matriz[2][2] = {1,0,0,2};
+  int cores[20][3] ={{0, 100, 80},{80, 0, 100},{100, 80, 0},{0, 80, 100},{100, 0, 80},{80, 100, 0},{0, 100, 100},{100, 0, 100},{100, 100, 0},
+  									{0, 80, 80},{80, 0, 80},{80, 80, 0},{0, 0, 80},{0, 80, 0},{80, 0, 0},{0, 0, 100},{0, 100, 0},{100, 0, 0},{200, 200, 200},
+  									{0, 80, 80}};
+  								
+  float matriz[2][2] = {1,0,0,2}; //variavel de teste
+  int indice;
+
 
   /*le o arquivo e preenche as variaveis*/
   arquivo = fopen("entrada.txt","r");
   fscanf(arquivo, "%d %f %f %f %f %e %d %e %d %d", &teste, &a, &b, &c, &d, &h, &itmax, &epsilon, &linhas, &colunas);
   fclose(arquivo);
 
+  /*Cria o arquivo de imagem e preenche o cabecalio*/
  	arquivo = fopen("imagem.ppm","w");
  	fprintf(arquivo,"P3 %d %d 255\n", colunas, linhas);
  	
-  /*itera a região do dominio a ser estudada*/
+  /*itera a regiao do dominio a ser estudada*/
   for(i=0;i<linhas;i++){
   	for(j=0;j<colunas;j++){
-  		if(i==0){
- 				fprintf(arquivo,"255 255 255 ");
-  		} else {
-  			fprintf(arquivo,"255 0 0 ");
-  		}
+  		indice = (i/(linhas/20))%20;    /*---ATENCAO--- Teste de cores, apagar */
+  		fprintf(arquivo, "%d %d %d ", cores[indice][0], cores[indice][1], cores[indice][2]);
   	}
   }
 
-  /*Teste das funcoes - ATENCAO - apagar*/
-  inverte_22(matriz, varteste);
-
   fclose(arquivo);
-  //printf("Finalizado, %f \n", varteste);
-  printf("[ %f  %f ] \n", varteste[0][0], varteste[0][1]);
-  printf("[ %f  %f ] \n", varteste[1][0], varteste[1][1]);
+  printf("Finalizado \n");
 
   return(0);
 }
